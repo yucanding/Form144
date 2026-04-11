0e6de90ebfa57479700032cc801fe4a870178b3d
yucanding <46375804+yucanding@users.noreply.github.com>
2026-04-11 12:00:24 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -28,7 +28,8 @@ EXCLUDE_KEYWORDS = ["restricted stock", "option", "rsu", "psu", "exercise", "div
                    "charitable", "charity", "rollover", "gift", "class b", "tax", "bonus",
                    "retained", "indirectly acquired", "share transfer", "liquidating distribution", "estate",
                    "foundation", "in kind", "pro rata", "convert", "esop", "preferred stock", "preferred shares",
-                   "business combination", "private shares", "consummation", "deed", "earnout"]
+                   "business combination", "private shares", "consummation", "deed", "earnout", "espp", "employee stock purchase",
+                   "transaction consideration"]
 
 def send_telegram(message, target_id):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
