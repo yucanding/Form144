1a8e4d9157413c28c538845f9e867de96a07bfc2
yucanding <46375804+yucanding@users.noreply.github.com>
2026-08-23 21:47:38 +0800

    Add new terms to the list in monitor.py

--- monitor.py
+++ monitor.py
@@ -31,7 +31,7 @@ EXCLUDE_KEYWORDS = ["restricted stock", "option", "rsu", "psu", "exercise", "div
                    "business combination", "private shares", "consummation", "deed", "earnout", "espp", "employee stock purchase",
                    "transaction consideration", "trust", "co-trustee", "situs", "affiliated", "received in",
                    "distribution", "spac", "private transaction", "founder", "pre-ipo", "ipo", "initial public offering",
-                   "conversion of preference shares", "founder shares"]
+                   "conversion of preference shares", "founder shares", "401", "inheritance", "equity incentive plan", "stock purchase plan"]
 
 def send_telegram(message, target_id):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
